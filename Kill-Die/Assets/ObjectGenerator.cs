c8c8a50c0d5d444bb40b3077808d6747947904ab
Gavruk12345 <game.master.kvx@gmail.com>
2023-12-23 19:21:10 +0200

    Add Sprites and Grafiks

---
+++ Kill-Die/Assets/ObjectGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ObjectParameters
+{
+    public GameObject prefab;
+    public int numObjects = 5;
+    public float maxDistance = 5f;
+}
+
+public class ObjectGenerator : MonoBehaviour
+{
+    public ObjectParameters[] objectParams;
+
+    void Start()
+    {
+        GenerateObjects();
+    }
+
+    void GenerateObjects()
+    {
+        foreach (var param in objectParams)
+        {
+            Vector3 basePosition = transform.position;
+
+            for (int i = 0; i < param.numObjects; i++)
+            {
+                float angle = i * 2f * Mathf.PI / param.numObjects;
+                float distance = Random.Range(0f, param.maxDistance);
+
+                float newX = basePosition.x + distance * Mathf.Cos(angle);
+                float newY = basePosition.y + distance * Mathf.Sin(angle);
+
+                Vector3 newPosition = new Vector3(newX, newY, basePosition.z);
+                Instantiate(param.prefab, newPosition, Quaternion.identity);
+            }
+        }
+    }
+}
