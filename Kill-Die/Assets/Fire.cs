92a73baa4fb4f84190eb7953a1f8f627dd0ee64a
Gavruk12345 <game.master.kvx@gmail.com>
2023-12-29 02:23:11 +0200

    Update Fire

---
+++ Kill-Die/Assets/Fire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public float delayInSeconds = 5f; // Час затримки перед знищенням
+
+    void Start()
+    {
+        // Викликаємо метод DestroyObject через зазначений час
+        Invoke("DestroyObject", delayInSeconds);
+    }
+
+    void DestroyObject()
+    {
+        // Знищення об'єкта
+        Destroy(gameObject);
+    }
+}
---
+++ Kill-Die/Assets/MolotovV2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MolotovV2 : MonoBehaviour
+{
+    public GameObject prefabToSpawn; // Префаб об'єкта, який з'явиться в новому місці
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Гравець доторкнувся до об'єкта
+
+            // Створення нового об'єкта на місці об'єкта Molotov
+            SpawnNewObject();
+
+            // Знищення поточного об'єкта
+            Destroy(gameObject);
+        }
+    }
+
+    private void SpawnNewObject()
+    {
+        if (prefabToSpawn != null)
+        {
+            // Створення нового об'єкта на тому самому місці, де розташований об'єкт Molotov
+            Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+        }
+    }
+}
